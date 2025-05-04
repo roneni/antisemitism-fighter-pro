3fc96a96743190088be2580732c5ae0d939d2772
roneni <katzronen1709@gmail.com>
2025-05-04 22:15:53 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import '../globals.css';
+
+export const metadata = {
+  title: 'Fight Antisemitism',
+  description: 'Instantly refute antisemitic claims with facts.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
